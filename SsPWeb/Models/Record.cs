b64acea63c8cb14e2acfded3da03ca6e888bb725
dadwonkulahjr <dad.wonkulah@comaxict.com>
2021-12-19 04:27:54 +0000

    Add properties in both model files!

--- SsPWeb/Models/Record.cs
+++ SsPWeb/Models/Record.cs
@@ -1,6 +1,18 @@
-﻿namespace SsPWeb.Models
+﻿using System;
+
+namespace SsPWeb.Models
 {
     public class Record
     {
+        public int StudentId { get; set; }
+        public string Address { get; set; }
+
+        public string LonestarNumber { get; set; }
+        public string OrangeNumber { get; set; }
+        public string WhatAppNumber { get; set; }
+
+        public DateTime? Dob { get; set; }
+
+        public virtual Student Student { get; set; }
     }
 }
